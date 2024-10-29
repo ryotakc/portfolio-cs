a9f2838adf664281d59810a3a24c2e4a06ca4012
Ryota Kato <rka147@sfu.ca>
2024-10-29 15:46:49 -0700

    edit Title style in Page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,14 +2,13 @@ import PageTitle from "@/components/page-title";
 import { ContactDock } from "./components/contact-dock";
 import { FlipWords } from "@/components/ui/flip-words";
 
-
 export default function Contact() {
     const words = ["Mail", "Instagram", "LinkedIn", "and More"];
     return (
-        <div className="container">
-            <div className="h-[20rem] pt-20">
+        <div className="container flex flex-col items-center mt-20 h-screen">
+            <div className="h-[20rem]">
                 <PageTitle>
-                    <div className="font-bold mb-2 lg:text-6xl text-4xl mx-auto text-neutral-600 dark:text-neutral-400">
+                    <div className="font-bold lg:text-6xl text-4xl mx-auto text-neutral-600 dark:text-neutral-400 text-center">
                         Contact with <span> </span>
                         <br className="sm:hidden"/>
                         <FlipWords words={words} className="text-primary/50" /> <br />
@@ -17,7 +16,6 @@ export default function Contact() {
                 </PageTitle>
                 <ContactDock />            
             </div>
-
         </div>
     );
 }
