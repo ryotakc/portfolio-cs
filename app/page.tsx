dd87269878228d5e238beed9a9d4fe1e8a2d66de
Ryota Kato <rka147@sfu.ca>
2024-10-29 15:09:22 -0700

    Fixed some words color, and size

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,19 @@ import { FileCheck } from "lucide-react";
 
 
 export default function Home() {
-  const words = ["Coding", "Photography", "Design"];
+  const words = ["Coding!", "Photography!", "Design!"];
   return (
 
     <div className="container" >
       <div className="h-[20rem] pt-20">
         <PageTitle>
           <div className="font-bold mb-2 lg:text-6xl text-4xl mx-auto text-neutral-600 dark:text-neutral-400">
-          I Love <span> </span>
+          Ryota Loves <span> </span>
           <br className="sm:hidden"/>
           <FlipWords words={words} className="text-primary/50" /> <br />
           </div>    
-          <p className="text-neutral-600 dark:text-neutral-400 pt-4">
-            Hi! Welcome to <span className="font-semibold" color="text-primary/50">Ryota Kato</span>&apos;s Portfolio! <br />
+          <p className="text-neutral-600 dark:text-neutral-400 pt-6">
+            Hi! Welcome to <span className="text-xl font-semibold text-blue-400"> Ryota Kato</span>&apos;s Portfolio! <br />
             I&apos;m a web developer and photographer based in Vancouver, Canada. I&apos;m a passionate coder and photographer. I love to create and capture beautiful things. Feel free to reach out to me! I&apos;m looking forward to hearing from you! 
           </p>
           <div className="flex space-x-3 py-5">
