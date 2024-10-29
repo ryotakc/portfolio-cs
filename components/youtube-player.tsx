d06b809f9775db144300b95217c888326258b90c
Ryota Kato <rka147@sfu.ca>
2024-10-29 14:56:40 -0700

    Fixed youtube link and thumbnail

--- components/youtube-player.tsx
+++ components/youtube-player.tsx
@@ -6,15 +6,15 @@ export function YoutubeDial() {
       <HeroVideoDialog
         className="dark:hidden block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+        videoSrc="https://www.youtube.com/watch?v=2CP_9-jCV6A&list=WL&index=102&pp=gAQBiAQB"
+        thumbnailSrc="https://i.ytimg.com/vi/2CP_9-jCV6A/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgzzdjeywXcZr6oksSjNF-VlZLlw"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        videoSrc="https://www.youtube.com/watch?v=2CP_9-jCV6A&list=WL&index=102&pp=gAQBiAQB"
+        thumbnailSrc="https://i.ytimg.com/vi/2CP_9-jCV6A/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgzzdjeywXcZr6oksSjNF-VlZLlw"
         thumbnailAlt="Hero Video"
       />
     </div>
