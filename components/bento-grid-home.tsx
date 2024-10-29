8e0a6890883f0af03400286caded6f4f4cb95d12
Ryota Kato <rka147@sfu.ca>
2024-10-29 14:08:12 -0700

    fixed skeltonone and two

--- components/bento-grid-home.tsx
+++ components/bento-grid-home.tsx
@@ -108,30 +108,33 @@ const FeatureDescription = ({ children }: { children?: React.ReactNode }) => {
 };
 
 export const SkeletonOne = () => {
-  return (
-    <div className="relative flex py-8 px-2 gap-10 h-full">
-      <div className="w-full p-5 mx-auto shadow-2xl group h-full">
-        <div className="flex flex-1 w-full h-full flex-col space-y-2">
-          <Image
-            src={`${BASE_PATH}/work/project1.webp`}
-            alt="header"
-            width={300}
-            height={200}
-            className="h-full w-full aspect-[4/3] object-cover object-left-top rounded-sm mb-10"
-          />
+    return (
+      <div className="relative flex py-2 px-2 gap-4 h-full">
+        <div className="w-full p-3 mx-auto shadow-2xl group h-full">
+          <div className="flex flex-1 w-full h-full flex-col space-y-2">
+            <Image
+              src={`${BASE_PATH}/work/project1.webp`}
+              alt="header"
+              width={180}
+              height={120}
+              className="w-full aspect-[3/2] object-cover rounded-md blur-none"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+  
+  
 
 export const SkeletonTwo = () => {
   const images = [
-    "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1555400038-63f5ba517a47?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "/photograph/pic1.jpg",
+    "/photograph/pic2.jpg",
+    "/photograph/pic3.jpg",
+    "/photograph/pic4.jpg",
+    "/photograph/pic5.jpg",
+    "/photograph/pic6.jpg",
   ];
 
   const [randomRotations, setRandomRotations] = useState<number[]>([]);
@@ -155,7 +158,29 @@ export const SkeletonTwo = () => {
             className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
           >
             <Image
-              src={image}
+              src={`${BASE_PATH}${image}`}
+              alt="bali images"
+              width="500"
+              height="500"
+              className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+            />
+          </motion.div>
+        ))}
+      </div>
+      // Row 2
+      <div className="flex flex-row">
+        {images.map((image, idx) => (
+          <motion.div
+            key={`images-first-${idx}`}
+            style={{
+              rotate: randomRotations[idx] || 0,
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
+            className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
+          >
+            <Image
+              src={`${BASE_PATH}${image}`}
               alt="bali images"
               width="500"
               height="500"
