e5bd8bccbc39968cf464a9bda806da2f11259b16
Ryota Kato <rka147@sfu.ca>
2024-10-29 01:44:08 -0700

    added description and instagram link

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -8,22 +8,22 @@ export default function Photography() {
     const words = ["Photography", "Retouch", "Editing"];
     return (
         <div className="container">
-            <div className="h-[20rem] flex items-center px-0 mt-10 ">
+            <div className="h-[20rem] mt-20 ">
                 <PageTitle>
                 <div className="font-bold mb-2 lg:text-6xl text-4xl mx-auto text-neutral-600 dark:text-neutral-400">
                 I Love <span> </span>
                 <br className="sm:hidden"/>
                 <FlipWords words={words} className="text-primary/50" /> <br />
                 </div>    
-                {/* <p className="text-neutral-600 dark:text-neutral-400 py-4">
+                <p className="text-neutral-600 dark:text-neutral-400 py-4">
                     Photography is an art form that captures the world around me, preserving moments in time forever. Our service offers professional skills and creative perspectives to beautifully document your precious moments.
-                </p> */}
-                {/* <div className="flex space-x-3 py-3">
+                </p>
+                <div className="flex space-x-3 py-5">
                     <HoverBorderGradient link="../contact">
                     <SiInstagram />
                     <span>Instagram</span>
                     </HoverBorderGradient>
-                </div>   */}
+                </div>  
                 </PageTitle>
             </div> 
         </div>
