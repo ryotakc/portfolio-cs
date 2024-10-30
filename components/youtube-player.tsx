936fecee717639efe27f2b0628374741778a390e
Ryota Kato <rka147@sfu.ca>
2024-10-29 18:57:30 -0700

    fixed youtube link

--- components/youtube-player.tsx
+++ components/youtube-player.tsx
@@ -6,14 +6,14 @@ export function YoutubeDial() {
       <HeroVideoDialog
         className="dark:hidden block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/watch?v=2CP_9-jCV6A&list=WL&index=102&pp=gAQBiAQB"
+        videoSrc="https://www.youtube.com/watch?v=2CP_9-jCV6A&ab_channel=typecraft"
         thumbnailSrc="https://i.ytimg.com/vi/2CP_9-jCV6A/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgzzdjeywXcZr6oksSjNF-VlZLlw"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/watch?v=2CP_9-jCV6A&list=WL&index=102&pp=gAQBiAQB"
+        videoSrc="https://www.youtube.com/watch?v=2CP_9-jCV6A&ab_channel=typecraft"
         thumbnailSrc="https://i.ytimg.com/vi/2CP_9-jCV6A/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgzzdjeywXcZr6oksSjNF-VlZLlw"
         thumbnailAlt="Hero Video"
       />
