12f68f6b3e40cce34c186db4dff0673ec8eaff38
Ryota Kato <rka147@sfu.ca>
2024-10-29 17:03:47 -0700

    Added budge component(Shadcn/UI) and edited images-slider-work

--- app/work/components/images-slider-work.tsx
+++ app/work/components/images-slider-work.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/components/ui/timeline";
+import { Badge } from "@/components/ui/badge";
 
 export function TimelineWork() {
   const data = [
     {
-      title: "2024",
+      title: "New Portfolio",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Built and launched Aceternity UI and Aceternity UI Pro from scratch
+            Build and launched new personal Portfolio website using Next.js, Tailwind CSS, and GitHub Pages.
           </p>
+          <div className="mb-6">
+              <Badge variant="outline">Next.js</Badge>
+              <Badge variant="outline">Tailwind CSS</Badge>
+              <Badge variant="outline">Aceternity UI</Badge>
+              <Badge variant="outline">GitHub Pages</Badge>
+              
+          </div>
           <div className="grid grid-cols-2 gap-4">
             <Image
               src="https://assets.aceternity.com/templates/startup-1.webp"
@@ -45,17 +53,66 @@ export function TimelineWork() {
       ),
     },
     {
-      title: "Early 2023",
+      title: "2024 Fall",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+            Started to use Arch Linux and Ubuntu as my daily driver. Try some dotfiles and rice my desktop.
+          </p>
+          <div className="mb-6">
+              <Badge variant="outline">Arch Linux</Badge>
+              <Badge variant="outline">Ubuntu</Badge>
+              <Badge variant="outline">NeoVim</Badge>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="https://assets.aceternity.com/pro/hero-sections.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="https://assets.aceternity.com/features-section.png"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="https://assets.aceternity.com/pro/bento-grids.png"
+              alt="bento template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="https://assets.aceternity.com/cards.png"
+              alt="cards template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "2024 Summer",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
+            Started to learn React, Next.js, and Tailwind CSS. Build some projects and templates.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Lorem ipsum is for people who are too lazy to write copy. But we are
-            not. Here are some more example of beautiful designs I built.
+            Launched first personal portfolio website using Next.js, Tailwind CSS, Shadcn/UI, and Vercel.
           </p>
+          <div className="mb-6">
+              <Badge variant="outline">Next.js</Badge>
+              <Badge variant="outline">Tailwind CSS</Badge>
+              <Badge variant="outline">Shadcn/UI</Badge>
+              <Badge variant="outline">Vercel</Badge>
+          </div>
           <div className="grid grid-cols-2 gap-4">
             <Image
               src="https://assets.aceternity.com/pro/hero-sections.png"
